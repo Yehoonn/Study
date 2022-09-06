f80d2191066a2b154831a3c6af17882a3c923aed
Yehoon Kang <jes6208@naver.com>
2022-09-07 01:25:02 +0900

    study

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import MyComponent from "./Component/MyComponent";
 
 function App() {
   const text = "리액트";
 
-  return <div className="div-text">{text}</div>;
+  return (
+    <>
+      <div className="div-text">{text}</div>
+      <MyComponent name="강예훈" name2="강예찬" />
+    </>
+  );
 }
 
 export default App;
